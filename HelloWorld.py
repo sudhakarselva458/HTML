3e58f31a2383f3a966e7116d150e46efaf9fdda0
sudhakarselva458 <sudhakarselva458@gmail.com>
2024-01-18 22:10:53 +0000

    update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,16 +1,19 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-      host="localhost",
-      user="root",
-      password="Sudhakar@95"
-
-
+mydb=mysql.connector.connect(
+    host="localhost",
+username="root",
+password="Sudhakar@95",
+database="mydatabase"
 )
 
-#print(mydb)
-#mycursor = mydb.cursor()
-#mycursor.execute("create database mydatabase")
-mycursor = mydb.cursor()
+"""for database
+mycursor=mydb.cursor()
 mycursor.execute("show databases")
+for x in mycursor:
+   print(x)
+mycursor=mydb.cursor()
+mycursor.execute("create table student(id int,name varchar(50),marks int)")"""
+mycursor=mydb.cursor()
+mycursor.execute("show tables")
 for x in mycursor:
     print(x)
