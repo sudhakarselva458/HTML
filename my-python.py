6a7d01a14bc2468c6d538d283ce5053529cc5751
sudhakarselva458 <sudhakarselva458@gmail.com>
2024-01-20 21:58:04 +0000

    update

--- my-python.py
+++ my-python.py
@@ -1,1 +1,8 @@
-git
+a=input("username:"),
+b=input("password:"),
+if(a=="sudhakar" and b=="selva"):
+    print("log in")
+    if(a=="sudhakar" and b!="selva"):
+        print("password entered wrong")
+else:
+    print("invalid user name ")
