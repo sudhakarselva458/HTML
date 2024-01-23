f6c5711f54689bacb28071c5aaf5f97fb0ba9b70
sudhakarselva458 <sudhakarselva458@gmail.com>
2024-01-23 18:30:13 +0000

    upadte

--- my-python.py
+++ my-python.py
@@ -1,8 +1,12 @@
-a=input("username:"),
-b=input("password:"),
+a=input("username:")
+b=input("password:")
+#a="sudhakar"
+#b="selva"
 if(a=="sudhakar" and b=="selva"):
     print("log in")
-    if(a=="sudhakar" and b!="selva"):
+elif(a=="sudhakar" or b!="selva"):
         print("password entered wrong")
+elif(a!="sudhakar" or b!="selva"):
+            print("invalid details")
 else:
     print("invalid user name ")
